a8d63383e7076a237638860fc59a133c3682de25
Adam C Hamlin <achamlin@gmail.com>
2021-10-25 23:08:34 -0400

    Add unit tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,158 @@
-import * as assert from 'assert';
 import * as vscode from 'vscode';
 import * as chai from 'chai';
+import * as dedent from 'dedent';
 import { toggleCommentType } from '../../commands/toggleCommentType';
-import { openFile } from '../utils/test-utils';
+import { openEditorWithContent } from '../utils/test-utils';
 
 const expect = chai.expect;
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+/**
+ * NOTE: Be sure to run these tests with --disable-extensions flag
+ */
 
-	test('Sample test', () => {
-		expect(true).to.be.true;
-	});
+describe('Extension Test Suite', () => {
+
+    afterEach(async () => {
+        await vscode.commands.executeCommand('workbench.action.closeAllEditors');
+    });
+
+    describe('toggleCommentType cycles from line comment, to block, to free text, back to line comment', () => {
+
+        it('javascript', async () => {
+            const editor = await openEditorWithContent('javascript', dedent`
+                // First line
+                // Second line
+                // Third line
+            `);
+            await toggleCommentType(editor);
+            expect(editor.document.getText(editor.selection)).to.equal(dedent`
+                /**
+                 * First line
+                 * Second line
+                 * Third line
+                 */
+            `);
+            await toggleCommentType(editor);
+            expect(editor.document.getText(editor.selection)).to.equal(dedent`
+                First line
+                Second line
+                Third line
+            `);
+            await toggleCommentType(editor);
+            expect(editor.document.getText(editor.selection)).to.equal(dedent`
+                // First line
+                // Second line
+                // Third line
+            `);
+        });
+
+        it('python', async () => {
+            const editor = await openEditorWithContent('python', dedent`
+                # First line
+                # Second line
+                # Third line
+            `);
+            await toggleCommentType(editor);
+            expect(editor.document.getText(editor.selection)).to.equal(dedent`
+                """
+                First line
+                Second line
+                Third line
+                """
+            `);
+            await toggleCommentType(editor);
+            expect(editor.document.getText(editor.selection)).to.equal(dedent`
+                First line
+                Second line
+                Third line
+            `);
+            await toggleCommentType(editor);
+            expect(editor.document.getText(editor.selection)).to.equal(dedent`
+                # First line
+                # Second line
+                # Third line
+            `);
+        });
+
+        it('shellscript', async () => {
+            // NOTE: No block comment for shellscript
+            const editor = await openEditorWithContent('shellscript', dedent`
+                # First line
+                # Second line
+                # Third line
+            `);
+            await toggleCommentType(editor);
+            expect(editor.document.getText(editor.selection)).to.equal(dedent`
+                First line
+                Second line
+                Third line
+            `);
+            await toggleCommentType(editor);
+            expect(editor.document.getText(editor.selection)).to.equal(dedent`
+                # First line
+                # Second line
+                # Third line
+            `);
+        });
+
+        it('unspecified language -- assume java-style', async () => {
+            const editor = await openEditorWithContent(undefined, dedent`
+                First line
+                Second line
+                Third line
+            `);
+            await toggleCommentType(editor);
+            expect(editor.document.getText(editor.selection)).to.equal(dedent`
+                // First line
+                // Second line
+                // Third line
+            `);
+            await toggleCommentType(editor);
+            expect(editor.document.getText(editor.selection)).to.equal(dedent`
+                /**
+                 * First line
+                 * Second line
+                 * Third line
+                 */
+            `);
+            await toggleCommentType(editor);
+            expect(editor.document.getText(editor.selection)).to.equal(dedent`
+                First line
+                Second line
+                Third line
+            `);
+        });
+
+        it('ruby + indented code', async () => {
+            function linesToStr(lines: string[]): string {
+                return lines.join('\n');
+            }
+
+            const editor = await openEditorWithContent('ruby', linesToStr([
+                '    # First line',
+                '    # Second line',
+                '    # Third line'
+            ]));
+            await toggleCommentType(editor);
+            expect(editor.document.getText(editor.selection)).to.equal(linesToStr([
+                '    =begin',
+                '    First line',
+                '    Second line',
+                '    Third line',
+                '    =end'
+            ]));
+            await toggleCommentType(editor);
+            expect(editor.document.getText(editor.selection)).to.equal(linesToStr([
+                '    First line',
+                '    Second line',
+                '    Third line'
+            ]));
+            await toggleCommentType(editor);
+            expect(editor.document.getText(editor.selection)).to.equal(linesToStr([
+                '    # First line',
+                '    # Second line',
+                '    # Third line'
+            ]));
+        });
+    });
 });
--- src/test/utils/test-utils.ts
+++ src/test/utils/test-utils.ts
@@ -1,8 +1,15 @@
 import * as vscode from 'vscode';
 
-// TODO: remove this if not using
-export async function openFile(content: string): Promise<vscode.TextDocument> {
-    const document = await vscode.workspace.openTextDocument(content);
-    vscode.window.showTextDocument(document);
-    return document;
+/**
+ * Open a text document with the given language and content, then make it the active document and
+ * select all the text therein.
+ *
+ * @param language the language for the document. Use undefined to not specify a language
+ * @param content the string content of the document
+ */
+export async function openEditorWithContent(language: string | undefined, content: string): Promise<vscode.TextEditor> {
+    const document = await vscode.workspace.openTextDocument({content, language});
+    const editor = await vscode.window.showTextDocument(document);
+    await vscode.commands.executeCommand('editor.action.selectAll');
+    return editor;
 }
